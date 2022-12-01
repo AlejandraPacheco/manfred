c10d1ba262d90dd6612fea9cfd0d56410435fa00
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2022-11-30 21:20:27 -0400

    Login sin backend

---
+++ lib/cubit/login_cubit.dart
@@ -0,0 +1,33 @@
+import '../dto/login_response_dto.dart';
+import '../service/login_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(const LoginState());
+
+  Future<void> login(String username, String password) async {
+    emit(state.copyWith(status: PageStatus.loading));
+    try {
+      LoginResponseDto response = await LoginService.login(username, password);
+      if (response.success) {
+        emit(state.copyWith(
+            loginSuccess: true,
+            status: PageStatus.success,
+            token: response.token,
+            refreshToken: response.refreshToken));
+      } else if (!response.success) {
+        emit(state.copyWith(
+            loginSuccess: false,
+            status: PageStatus.success,
+            errorMessage: "Usuario o contraseña incorrectos"));
+      }
+    } on Exception catch (ex) {
+      emit(state.copyWith(
+          loginSuccess: false,
+          status: PageStatus.failure,
+          errorMessage: "Error al intentar autenticar al usuario",
+          exception: ex));
+    }
+  }
+}
